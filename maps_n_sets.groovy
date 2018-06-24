fb0203fb86fbdbf6d59852f5cd45d0f4fbe99301
Alex <alex.d.bondarev@gmail.com>
2018-06-24 21:02:16 +0300

    add maps and sets example

---
+++ maps_n_sets.groovy
@@ -0,0 +1,23 @@
+def map = [a:1, b:2, c:3, 'p':0]
+map.put('d', 4)
+map['e'] = 5
+println map
+println map.getClass().name
+
+def list = [1, 2, 3, 4, 5, 1, 1] as LinkedList
+println list
+println list.class.name
+
+def set = [1, 1, 3, 4, 5, 2, 1] as SortedSet
+println set.descendingSet()
+println set.class.name
+
+//def strings = "this is a list of strings".split()
+List strings = "this is a list of strings".split()
+println strings
+println strings.class.name
+
+List<Integer> integers = [1, 2, 3, 4, 'abc']
+integers << new Date()
+println integers
+println integers.class.name
