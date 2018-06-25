c41f8166a65bbe2c9da6f879fee8a905a70900d9
Alex <alex.d.bondarev@gmail.com>
2018-06-25 21:40:08 +0300

    add closure basics

--- closure_iteration.groovy
+++ closure_iteration.groovy
@@ -1,1 +1,16 @@
 
+//List nums = [1, 2, 9, 8, 7, 5, 3, 1]
+//nums.each { num -> println num }
+//nums.eachWithIndex { num, idx -> println num + " is " + idx}
+
+//Map m = [a:1, b:2, c:3, p:0]
+//for (String key : m.keySet()){
+//    def val = m[key]
+//    println "map[$key] = $val"
+//}
+//m.each { e -> println "entry is $e" }
+//m.each { k, v -> println "key $k is $v" }
+
+//10.downto(7, { println it })
+10.downto(7) { println it }
+//10.downto 7, { println it }
