5586c336340e7db3f1893684bed0a8b57293e1eb
Alex <alex.d.bondarev@gmail.com>
2018-06-26 23:38:34 +0300

    add parse json example

---
+++ icndb.groovy
@@ -0,0 +1,11 @@
+
+import groovy.json.*
+
+String baseURL = "http://api.icndb.com/jokes/random?"
+def queryString = [limitTo:'[nerdy]', firstName:'Guillaume', lastName:'Laforge']
+        .collect { k, v -> "$k=$v" }
+        .join('&')
+
+String jsonText = "$baseURL$queryString".toURL().text
+def json = new JsonSlurper().parseText(jsonText)
+println json.value.joke
